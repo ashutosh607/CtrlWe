440e63262f9af4e54089883457aa90075c09f875
Ashutosh <ashutoshkadam607@gmail.com>
2026-02-14 12:22:06 +0530

    added-js

---
+++ script.js
@@ -0,0 +1,30 @@
+const scrollBtn = document.getElementById("scroll")
+
+scrollBtn.addEventListener("click", function () {
+    document.getElementById("dashboard").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+const signinBtn = document.querySelector(".signin-btn");
+const overlay = document.querySelector(".overlay");
+const signinModal = document.querySelector(".signin-modal");
+
+// Open modal
+signinBtn.addEventListener("click", function () {
+    overlay.classList.add("active");
+    signinModal.classList.add("active");
+});
+
+// Close when clicking outside
+overlay.addEventListener("click", function () {
+    overlay.classList.remove("active");
+    signinModal.classList.remove("active");
+});
+
+// Close with ESC
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape") {
+        overlay.classList.remove("active");
+        signinModal.classList.remove("active");
+    }
+});
